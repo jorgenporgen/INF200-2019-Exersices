345300d9357eee686d3d173e6d0ac8a4f27990cb
Jørgen Kongsro <jorjoh@nmbu.no>
2019-09-13 09:08:49 +0200

    Excercise_01 - ex_01. Setup 4 files and changed README.rst and setup.cfg

---
+++ src/comp_to_loop.py
@@ -0,0 +1,2 @@
+def squares_by_comp(n):
+    return [k**2 for k in range(n) if k % 3 == 1]
---
+++ src/deck_by_comp.py
@@ -0,0 +1,16 @@
+SUITS = ('C', 'S', 'H', 'D')
+VALUES = range(1, 14)
+
+def deck_loop():
+    deck = []
+    for suit in SUITS:
+        for val in VALUES:
+            deck.append((suit, val))
+    return deck
+
+def deck_comp():
+    pass
+
+if __name__ == '__main__':
+    if deck_loop() != deck_comp():
+        print('ERROR!')
