539973e172108a7f1753f206e569f4bc0f4b68a1
Jørgen Kongsro <jorjoh@nmbu.no>
2019-09-27 13:58:26 +0200

    Last update.
    I have tried my best. Unfortunately, I was not able to complete message_entropy, since I struggled with calculating entropy.

--- src/YOUR_NAME_ex/ex02/bubble_sort.py
+++ src/YOUR_NAME_ex/ex02/bubble_sort.py
@@ -1,15 +1,22 @@
+""" This module is part of the ex_02 assignment """
+
 def bubble_sort(iterable):
+    """ Bubble sort is a simple sorting algorithm
+
+    Returns
+    -------
+    data : list
+    """
     data = list(iterable)
     sorting_lenght = len(data) - 1
     for i in range(sorting_lenght):
         for j in range(sorting_lenght - i):
-            if data[j]>data[j+1]:
-                data[j],data[j+1] = data[j+1], data[j]
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
     return data
 
 
 if __name__ == "__main__":
-    for data in ((),(1,),(1, 3, 8, 12),(12, 8, 3, 1),(8, 3, 12, 1)):
+    for data in ((), (1, ), (1, 3, 8, 12), (12, 8, 3, 1), (8, 3, 12, 1)):
         print('{!s:>15} --> {!s:>15}'.format(data, bubble_sort(data)))
-        
-        
+                
--- src/YOUR_NAME_ex/ex02/message_entropy.py
+++ src/YOUR_NAME_ex/ex02/message_entropy.py
@@ -1,7 +1,8 @@
 """ This module is part of the ex_02 assignment """
 
-from collections import Counter, defaultdict
+from collections import Counter
 from math import log2
+
 
 def letter_freq(txt):
     """ Function from ex_01
@@ -12,25 +13,24 @@ def letter_freq(txt):
 
 n_i = letter_freq('aava')
 
+
 def entropy(message):
     """ returns the entropy calculated according to the equation above
     Returns:
-        H:
+        H: list
     """
     N = len(message) # number of letters in message
     n_i = letter_freq(message) # number of occurences of letter i
     code_points = [ord(character) for character in message] # get code_points
     p_i = [n_i[chr(code_point)]/N for code_point in code_points]
-    
+
+    # Calculate entropy. I was not successfull in calculating entropy
+    H = []
+    h = lambda p_i: p_i + log2(p_i)
     for i in p_i:
-        H = p_i[i] + log2(p_i[i]) # calculate entropy
-    
-    #h = lambda p_i: p_i + log2(p_i)
-
-    #for i in n_i:
-    #    H[i] -= h
-
+        H -= h
     return H
+
 
 if __name__ == "__main__":
     for msg in '', 'aaaa', 'aaba', 'abcd', 'This is a short text.':
