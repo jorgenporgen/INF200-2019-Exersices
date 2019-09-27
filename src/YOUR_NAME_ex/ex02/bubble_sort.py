140f93a0255a114a6f5c46d3ecb2005c365cd67a
Jørgen Kongsro <jorjoh@nmbu.no>
2019-09-27 10:11:36 +0200

    update

--- src/YOUR_NAME_ex/ex02/bubble_sort.py
+++ src/YOUR_NAME_ex/ex02/bubble_sort.py
@@ -1,4 +1,8 @@
 def bubble_sort(data):
+    for i in data:
+        res = data[i]-data[i+1]
+
+    
     a=sorted(data)
     return a
 
@@ -6,3 +10,13 @@ def bubble_sort(data):
 if __name__ == "__main__":
     for data in ((),(1,),(1, 3, 8, 12),(12, 8, 3, 1),(8, 3, 12, 1)):
         print('{!s:>15} --> {!s:>15}'.format(data, bubble_sort(data)))
+        
+        
+        
+Start with the first element in the list, compare it with the second.
+If they are in wrong order, exchange them.
+Now compare the second and third element and exchange them if necessary.
+Proceed with third and fourth, fourth and fifth, etc, until you have compared and, if necessary, swapped the second-last and last elements.
+At this point, the last element in the list will be the largest element in the list (why?).
+Now start again from the beginning of the list, but stop once you reach the second-last element in the list.
+Then, start again from the begining of the list, but stop once you reach the third-last element in the list, etc, until just the first and second element are left to compare.
--- src/YOUR_NAME_ex/ex02/mesage_entropy.py
+++ src/YOUR_NAME_ex/ex02/mesage_entropy.py
@@ -4,17 +4,30 @@ def letter_freq(txt):
     # Best solution
     return Counter(txt.lower())
 
+def count_occurences(s, c):
+    """ Count the occurences of a certain letter
+    """
+      
+    res = 0
+      
+    for i in range(len(s)): 
+          
+        # Checking character in string 
+        if (s[i] == c): 
+            res = res + 1
+    return res 
 
 def entropy(message):
     """ returns the entropy calculated according to the equation above
     """
     N = letter_freq(message)# number of letters in messsage
-    n_i # number of occurences of letter i (i is the UTF-8 code for the letter)
+    n_i = count_occurences(message, 'i')# number of occurences of letter i (i is the UTF-8 code for the letter)
     p_i = n_i/N #frequency of the letter in the message
     
-    H = - \sum_i p_i \log_2 p_i
+    H = - sum(p_i )
+    - \sum_i p_i \log_2 p_i
 
 
-   if __name__ == "__main__":
-       for msg in '', 'aaaa', 'aaba', 'abcd', 'This is a short text.':
-           print('{:25}: {:8.3f} bits'.format(msg, entropy(msg)))
+if __name__ == "__main__":
+    for msg in '', 'aaaa', 'aaba', 'abcd', 'This is a short text.':
+        print('{:25}: {:8.3f} bits'.format(msg, entropy(msg)))
