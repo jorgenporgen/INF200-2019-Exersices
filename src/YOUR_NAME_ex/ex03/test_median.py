3f56c8b4e2da64aeb20d0db4496c7e1ffe63b8b0
Jørgen Kongsro <jorjoh@nmbu.no>
2019-09-30 14:43:52 +0200

    Update

--- src/YOUR_NAME_ex/ex03/test_median.py
+++ src/YOUR_NAME_ex/ex03/test_median.py
@@ -18,7 +18,36 @@ def median(data):
             else 0.5 * (sdata[n // 2 - 1] + sdata[n // 2]))
 
 
- def test_median_for_one_element_list():
-     data = [12]
-     assert median(data) == 12
+def test_median_for_one_element_list():
+    assert median([1])
 
+
+def test_median_for_odd_numbers():
+    odd_data = [1, 3, 5, 9]
+    assert median(odd_data)
+
+
+def test_median_for_even_numbers():
+    even_data = [2, 4, 6, 8, 10]
+    assert median(even_data)
+
+
+def test_median_multiple_elements():
+    assert median([1])
+
+
+def test_median_value_error_exeption():
+    assert median([1])
+
+
+def test_median_leaves_orginial_unchanged():
+    assert median([1])
+
+
+def test_median_tuples_and_lists():
+    assert median([1])
+
+
+if __name__ == "__main__":
+    test_median_for_one_element_list()
+    print("Everything passed")
